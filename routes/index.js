ba0ad9856c37e3a8bfea3434536d1fcec460700f
juliettaurdiales@yahoo.comlsgit statusgit commit README.mdcd /var/www/mtbclsgit config --global user.name juliettaurdiales@yahoo.com <juliettaurdiales@yahoo.com>
2018-02-06 13:19:50 -0600

    testing logic for pm2

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Running with PM2', name: 'Julietta'});
 });
 
+///crash server fort pm2 texting
+router.get('/exit', function(req, res, next){
+  process.exit(1);
+});
+
+
 module.exports = router;
